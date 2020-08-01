b4162bad96ffc18476fb7ca0a3a539198b4e63bd
Karthikvenugopal <karthik.cs18@bmsce.ac.in>
2020-08-01 16:05:50 +0530

    audio working

--- src/render.js
+++ src/render.js
@@ -1,92 +1,97 @@
-const { desktopCapturer, remote } = require('electron')
+const { desktopCapturer, remote } = require("electron");
 
-const { writeFile } = require('fs')
+const { writeFile } = require("fs");
 
-const { dialog, Menu } = remote
+const { dialog, Menu } = remote;
 
-let mediaRecorder
-const recordedChunks = []
+let mediaRecorder;
+const recordedChunks = [];
 
-const videoElement = document.querySelector('video')
+const videoElement = document.querySelector("video");
 
-const startBtn = document.getElementById('startBtn')
-startBtn.onclick = e => {
-  mediaRecorder.start()
-  startBtn.classList.add('is-danger')
-  startBtn.innerText = 'Recording'
-}
+const startBtn = document.getElementById("startBtn");
+startBtn.onclick = (e) => {
+  mediaRecorder.start();
+  startBtn.classList.add("is-danger");
+  startBtn.innerText = "Recording";
+};
 
-const stopBtn = document.getElementById('stopBtn')
+const stopBtn = document.getElementById("stopBtn");
 
-stopBtn.onclick = e => {
-  mediaRecorder.stop()
-  startBtn.classList.remove('is-danger')
-  startBtn.innerText = 'Start'
-}
+stopBtn.onclick = (e) => {
+  mediaRecorder.stop();
+  startBtn.classList.remove("is-danger");
+  startBtn.innerText = "Start";
+};
 
-const videoSelectBtn = document.getElementById('videoSelectBtn')
-videoSelectBtn.onclick = getVideoSources
+const videoSelectBtn = document.getElementById("videoSelectBtn");
+videoSelectBtn.onclick = getVideoSources;
 
 async function getVideoSources() {
   const inputSources = await desktopCapturer.getSources({
-    types: ['window', 'screen']
-  })
+    types: ["window", "screen"],
+  });
 
   const videoOptionsMenu = Menu.buildFromTemplate(
-    inputSources.map(source => {
+    inputSources.map((source) => {
       return {
         label: source.name,
-        click: () => selectSource(source)
-      }
+        click: () => selectSource(source),
+      };
     })
-  )
+  );
 
-  videoOptionsMenu.popup()
+  videoOptionsMenu.popup();
 }
 
 async function selectSource(source) {
-  videoSelectBtn.innerText = source.name
+  videoSelectBtn.innerText = source.name;
 
   const constraints = {
-    audio: false,
+    audio: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+      },
+    },
     video: {
       mandatory: {
-        chromeMediaSource: 'desktop',
-        chromeMediaSourceId: source.id
-      }
-    }
-  }
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
 
-  const stream = await navigator.mediaDevices.getUserMedia(constraints)
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-  videoElement.srcObject = stream
-  videoElement.play()
+  videoElement.srcObject = stream;
+  videoElement.muted = true;
+  videoElement.play();
 
-  const options = { mimeType: 'video/webm; codecs=vp9' }
-  mediaRecorder = new MediaRecorder(stream, options)
+  const options = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(stream, options);
 
-  mediaRecorder.ondataavailable = handleDataAvailable
-  mediaRecorder.onstop = handleStop
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
 }
 
 function handleDataAvailable(e) {
-  console.log('video data available')
-  recordedChunks.push(e.data)
+  console.log("video data available");
+  recordedChunks.push(e.data);
 }
 
 async function handleStop(e) {
   const blob = new Blob(recordedChunks, {
-    type: 'video/webm; codecs=vp9'
-  })
+    type: "video/webm; codecs=vp9",
+  });
 
-  const buffer = Buffer.from(await blob.arrayBuffer())
+  const buffer = Buffer.from(await blob.arrayBuffer());
 
   const { filePath } = await dialog.showSaveDialog({
-    buttonLabel: 'Save video',
-    defaultPath: `vid-${Date.now()}.webm`
-  })
+    buttonLabel: "Save video",
+    defaultPath: `vid-${Date.now()}.webm`,
+  });
 
   if (filePath) {
-    writeFile(filePath, buffer, () => console.log('video saved successfully!'))
+    writeFile(filePath, buffer, () => console.log("video saved successfully!"));
   }
 }
